c5fa22859fedc3fdf1373aab89584ad313f2547e
Carlo Maurer <h.carlo.maurer@gmail.com>
2022-01-12 10:43:05 +0100

    SplitViolin Jan 12

--- cbviz/cbviz/KDE.py
+++ cbviz/cbviz/KDE.py
@@ -2,27 +2,51 @@
 import numpy as np
 import pandas as pd
 from scipy.stats import gaussian_kde
-from collections import namedtuple
+
 
 """Functions for univariate distributions"""
+
+class SplitViolin:
     
-def _fit(fit_data:np.ndarray, 
-         fit_kw: dict = None):
+    """Container for producing a split violin plot and compute associated statistics"""
+
+    def __init__(self, data:pd.DataFrame) -> None:
+        pass
+
+
+
+
+
+# These are mostly helper functions/classes
+
+def _kde_support(x: np.ndarray, 
+                fit_kw: dict = None,
+                **support_kwargs):
+    """Create a 1D grid of evaluation points."""
+    kde = _fit_kde(x, fit_kw=fit_kw)
+    bw = np.sqrt(kde.covariance.squeeze())
+    grid = _define_support_grid(x, bw, **support_kwargs)
+    
+    return grid
+
+def _fit_kde(x:np.ndarray, 
+             fit_kw: dict = None):
+    """Fit a gaussian KDE to numeric variable and adjust bandwidth."""
     fit_props = {"bw_method": "scott"}
     
     if fit_kw:
         fit_props.update(fit_kw)
         
-    kde = gaussian_kde(fit_data, **fit_props)
+    kde = gaussian_kde(x, **fit_props)
     kde.set_bandwidth(kde.factor * 1)
 
     return kde
 
 def _define_support_grid(x:np.ndarray, 
-                         bw:float, 
-                         cut:int=3, 
-                         clip:tuple = None, 
-                         gridsize:int = 200):
+                  bw:float, 
+                  cut:int=3, 
+                  clip:tuple = None, 
+                  gridsize:int = 200):
     """Create the grid of evaluation points depending for vector x."""
     if clip is None:
         clip = None, None
@@ -33,11 +57,3 @@ def _define_support_grid(x:np.ndarray,
     
     return np.linspace(gridmin, gridmax, gridsize)
 
-def _define_support_univariate(x: np.ndarray, 
-                               fit_kw: dict = None):
-    """Create a 1D grid of evaluation points."""
-    kde = _fit(x, fit_kw=fit_kw)
-    bw = np.sqrt(kde.covariance.squeeze())
-    grid = _define_support_grid(x, bw)
-    
-    return grid
