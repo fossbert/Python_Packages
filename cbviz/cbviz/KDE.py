61ef57b4655f08704589f0262d8b59375221a234
Carlo Maurer <h.carlo.maurer@gmail.com>
2022-01-12 15:48:24 +0100

    SplitViolin growing

--- cbviz/cbviz/KDE.py
+++ cbviz/cbviz/KDE.py
@@ -1,6 +1,7 @@
 # import
 import numpy as np
 import pandas as pd
+from itertools import product
 from scipy.stats import gaussian_kde
 
 
@@ -10,10 +11,77 @@ class SplitViolin:
     
     """Container for producing a split violin plot and compute associated statistics"""
 
-    def __init__(self, data:pd.DataFrame) -> None:
-        pass
+    def __init__(self, data:pd.DataFrame, s1_order: list = None, s2_order: list = None, scale_factor:int = 2) -> None:
+        
+        self.dtype_options = [('float', *i) for i in product(['object', 'category'], repeat=2)]
+        self.scale_factor = scale_factor
+        
+        
+        # First round of checks tests input DataFrame
+        # it's this complicated because the order of variables matters
+        # i.e. float goes first, then two object|category variables
+        
+        if isinstance(data, pd.DataFrame):            
+            if any([all(data.dtypes == list(dt_opt)) for dt_opt in self.dtype_options]):
+                    self.ylabel, self.s1, self.s2 = data.columns
+                    _, self.s1_dtype, self.s2_dtype = data.dtypes
+                    self.data_in = data
+            else:    
+                raise ValueError(f"DataFrame needs three columns, dtypes allowed are: float - object|category - object|category")
+        else:
+            raise ValueError('Need a pandas DataFrame')
+        
+        # Second checks the split variables: 
+        # 1.) Do they need conversion ? 
+        # 2.) s2 can only have two levels 
+        # 3.) If order is provided, reorder if provided levels match
+        if self.s1_dtype == 'object':
+            self.data_in[self.s1] = self.data_in[self.s1].astype('category')
+        if self.s2_dtype == 'object':
+            self.data_in[self.s2] = self.data_in[self.s2].astype('category')
+        
+        self.s1_categories = self.data_in[self.s1].cat.categories.to_list()
+        self.s2_categories = self.data_in[self.s2].cat.categories.to_list()
+        
+        if s2_ncat := len(self.s2_categories) != 2:
+            raise ValueError(f'Need 2 levels for {self.s2}, found {s2_ncat}') 
+        
+        if s1_order:
+             if all([s1_lvl in self.s1_categories for s1_lvl in s1_order]):
+                 self.data_in[self.s1] = self.data_in[self.s1].cat.reorder_categories(s1_order)
+                 self.s1_categories = s1_order
+             else:
+                 raise ValueError(f'Could not align levels, provided: {", ".join(s1_order)}, available: {", ".join(self.s1_categories)}')
+        
+        if s2_order:
+            if all([s2_lvl in self.s2_categories for s2_lvl in s2_order]):
+                self.data_in[self.s2] = self.data_in[self.s2].cat.reorder_categories(s2_order)
+                self.s2_categories = s2_order
+            else:
+                raise ValueError(f'Could not align levels, provided: {", ".join(s2_order)}, available: {", ".join(self.s2_categories)}')
+    
+        self.densities = self._get_grid_and_densities(self.data_in, self.ylabel, self.s1, self.s2)
+    
+    
+    def __repr__(self) -> str:
+        return (
+            f"SplitViolin(Numeric variable: {self.ylabel}\n "
+            f"Split 1: {self.s1}, dtype: {self.s1_dtype}, levels: {len(self.s1_categories)}\n"
+            f"Split 2: {self.s2}, dtype: {self.s2_dtype}, levels: {len(self.s2_categories)}\n "
+            f"Total data points: {len(self.data_in)})"
+        )
 
+    def _get_grid_and_densities(self, data: pd.DataFrame, x:str, s1:str, s2:str) -> pd.DataFrame:
 
+        return data.groupby([s1, s2]).apply(self._get_curves, x=x)
+                
+      
+    def _get_curves(self, grouped_df:pd.DataFrame, x:str):       
+        xvals = grouped_df[x].values
+        support = _kde_support(xvals)
+        kde = _fit_kde(xvals)
+        density = kde(support)
+        return pd.DataFrame({'grid':support, 'density':density})
 
 
 
