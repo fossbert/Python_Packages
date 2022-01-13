a6a2e355520fb5c15c5ce92f15f5f2eb73879bde
Carlo Maurer <h.carlo.maurer@gmail.com>
2022-01-13 23:07:43 +0100

    SplitViolin v0.1.0

--- cbviz/cbviz/KDE.py
+++ cbviz/cbviz/KDE.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 from itertools import product
 from scipy.stats import gaussian_kde
+from collections import namedtuple
+
+
 
 
 """Functions for univariate distributions"""
@@ -11,11 +14,20 @@ class SplitViolin:
     
     """Container for producing a split violin plot and compute associated statistics"""
 
-    def __init__(self, data:pd.DataFrame, s1_order: list = None, s2_order: list = None, scale_factor:int = 2) -> None:
+    def __init__(self, data:pd.DataFrame,  
+                scale_factor:int = 2,   
+                minsize:int = 5,    
+                s1_order: list = None, 
+                s2_order: list = None, 
+                s2_colors: tuple = None) -> None:
         
         self.dtype_options = [('float', *i) for i in product(['object', 'category'], repeat=2)]
         self.scale_factor = scale_factor
-        
+        self.minsize = minsize
+        if s2_colors:
+            self.s2_colors = s2_colors
+        else:
+            self.s2_colors = 'C0', 'C1'
         
         # First round of checks tests input DataFrame
         # it's this complicated because the order of variables matters
@@ -25,7 +37,7 @@ class SplitViolin:
             if any([all(data.dtypes == list(dt_opt)) for dt_opt in self.dtype_options]):
                     self.ylabel, self.s1, self.s2 = data.columns
                     _, self.s1_dtype, self.s2_dtype = data.dtypes
-                    self.data_in = data
+                    self.data_in = data.copy()
             else:    
                 raise ValueError(f"DataFrame needs three columns, dtypes allowed are: float - object|category - object|category")
         else:
@@ -33,18 +45,23 @@ class SplitViolin:
         
         # Second checks the split variables: 
         # 1.) Do they need conversion ? 
-        # 2.) s2 can only have two levels 
+        # 2.) s2 can only have two levels, s1 needs at least one level
         # 3.) If order is provided, reorder if provided levels match
         if self.s1_dtype == 'object':
             self.data_in[self.s1] = self.data_in[self.s1].astype('category')
+            self.s1_dtype = self.data_in[self.s1].dtype
         if self.s2_dtype == 'object':
             self.data_in[self.s2] = self.data_in[self.s2].astype('category')
+            self.s2_dtype = self.data_in[self.s2].dtype
         
         self.s1_categories = self.data_in[self.s1].cat.categories.to_list()
         self.s2_categories = self.data_in[self.s2].cat.categories.to_list()
         
         if s2_ncat := len(self.s2_categories) != 2:
             raise ValueError(f'Need 2 levels for {self.s2}, found {s2_ncat}') 
+
+        if len(self.s1_categories) < 1:
+            raise ValueError(f'There is not a single level for {self.s1}') 
         
         if s1_order:
              if all([s1_lvl in self.s1_categories for s1_lvl in s1_order]):
@@ -60,14 +77,21 @@ class SplitViolin:
             else:
                 raise ValueError(f'Could not align levels, provided: {", ".join(s2_order)}, available: {", ".join(self.s2_categories)}')
     
+        # Third check: need to make sure every combination of factors occurs often enough 
+        self.minsize_observed = self.data_in.groupby([self.s1, self.s2]).count().values.ravel().min()
+        if  self.minsize_observed < self.minsize:
+            raise AssertionError(f"Need at least {self.minsize} observations per subgroup, found minimum of {self.minsize_observed }!")
+
+        # Once we made it past all these checks, we can start with some calculations
         self.densities = self._get_grid_and_densities(self.data_in, self.ylabel, self.s1, self.s2)
-    
+
+        self.xtick_positions = range(0, len(self.s1_categories)*self.scale_factor, 2)
     
     def __repr__(self) -> str:
         return (
             f"SplitViolin(Numeric variable: {self.ylabel}\n "
-            f"Split 1: {self.s1}, dtype: {self.s1_dtype}, levels: {len(self.s1_categories)}\n"
-            f"Split 2: {self.s2}, dtype: {self.s2_dtype}, levels: {len(self.s2_categories)}\n "
+            f"Split 1: {self.s1}, levels: {len(self.s1_categories)}\n"
+            f"Split 2: {self.s2}, levels: {len(self.s2_categories)}\n "
             f"Total data points: {len(self.data_in)})"
         )
 
@@ -83,9 +107,34 @@ class SplitViolin:
         density = kde(support)
         return pd.DataFrame({'grid':support, 'density':density})
 
+    def get_violins(self):
+        # yield kde curves one by one 
+
+        for i, (s1, subdf1) in enumerate(self.densities.groupby(level=0)):
+            for j, (s2, subdf2) in enumerate(subdf1.groupby(level=1)):
+
+                grid = subdf2['grid'].values
+                name = '_'.join([s1, s2])
+                density = subdf2['density'].div(subdf2['density'].max()).values
+                if j%2==0:
+                    density =  density*-1 + i * self.scale_factor
+                    color = self.s2_colors[0]
+                    mode_y = np.repeat(grid[density.argmin()], 2)
+                else:
+                    density =  density + i * self.scale_factor
+                    color = self.s2_colors[1]
+                    mode_y = np.repeat(grid[density.argmax()], 2)
+                mode_x = np.array([np.min(density), np.max(density)])
+              
+                yield KdeData(name, color, grid, density, Mode(mode_x, mode_y))
 
 
 # These are mostly helper functions/classes
+
+KdeData = namedtuple('KdeData', "name color grid density mode")
+
+Mode = namedtuple('Mode', "xcoords ycoords")
+
 
 def _kde_support(x: np.ndarray, 
                 fit_kw: dict = None,
@@ -124,4 +173,3 @@ def _define_support_grid(x:np.ndarray,
     gridmax = min(x.max() + bw * cut, clip_hi)
     
     return np.linspace(gridmin, gridmax, gridsize)
-
