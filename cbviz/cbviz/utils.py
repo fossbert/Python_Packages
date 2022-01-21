a78a0dc8f02296ff4e1e0a0a77412203dde3b9f6
Carlo Maurer <h.carlo.maurer@gmail.com>
2022-01-21 14:33:25 +0100

    DataMix

--- cbviz/cbviz/utils.py
+++ cbviz/cbviz/utils.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
-from pandas.api.types import infer_dtype #, is is_string_dtype, is_categorical_dtype
 
+# type checking
+from pandas.api.types import infer_dtype 
+from itertools import product
+# as usual
 from collections import namedtuple
 
 """Functions and classes used by most other modules"""
@@ -39,6 +42,33 @@ class DataNum:
         if not all(expected == observed):
             raise TypeError(f"Expected {len(expected)} numeric columns, got {np.sum(observed=='floating')}")
 
-            
+class DataMix(DataNum):
+
+    """[This class prepares and checks DataFrames containing a numeric and one or two categorical variables]
+    """
+
+    def __init__(self, data:pd.DataFrame, ncat: int = None) -> None:
         
+        super()._check_df(data)
+        self.ncat = ncat
+        self._check_dtypes(data, self.ncat)
+        self.df = data
+        self.var_names = self.df.columns.to_list()
+        self.nans = (len(self.df) - self.df.count()).sum()
+
+    def __repr__(self) -> str:
+        return super().__repr__()
  
+    def _check_dtypes(self, data, ncat):
+        
+        expected_list = [(x, *y) for x in ['floating'] for y in list(product(['string', 'categorical'], repeat=ncat))]
+        observed = data.apply(infer_dtype).values
+        
+        if ncat+1 != len(observed):
+            raise ValueError(f'Number of columns expected: {len(ncat+1)}, got: {len(observed)}')
+        
+        if not any([all(np.array(exp) == observed) for exp in expected_list]):
+            raise TypeError(f"Could not verify data types, need: floating then {ncat} * string|categorical")
+
+            
+
