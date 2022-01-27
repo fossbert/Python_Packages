17b77c53273b0ac8662624f8edeae8fdc53789c8
Carlo Maurer <h.carlo.maurer@gmail.com>
2022-01-27 12:00:28 +0100

    dp in init

--- cbviz/cbviz/__init__.py
+++ cbviz/cbviz/__init__.py
@@ -4,5 +4,5 @@ The cbviz package provides classes and functions for visualization.
 
 
 from .kdetools import Ridge, SplitViolin
-from .xytools import XYview, XYpairs, XYzoom
+from .xytools import XYview, XYpairs, XYzoom, Dotplot
 from .bptools import StripBox
