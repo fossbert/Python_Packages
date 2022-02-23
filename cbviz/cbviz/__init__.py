ca0987f0cb8fecdd2f1eb0e7dd5314abf2b5d449
Carlo Maurer <h.carlo.maurer@gmail.com>
2022-02-23 11:58:27 +0100

    Forgot Corrplot in init

--- cbviz/cbviz/__init__.py
+++ cbviz/cbviz/__init__.py
@@ -6,4 +6,5 @@ The cbviz package provides classes and functions for visualization.
 from .kde import Ridge, SplitViolin
 from .xytools import XYview, XYpairs, XYzoom
 from .boxplot import StripBox
-from .dotplot import Dotplot
+from .dotplot import Dotplot, Corrplot
+
