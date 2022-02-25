1e6496501118b45045af4fe266085d296dda1afc
Carlo Maurer <h.carlo.maurer@gmail.com>
2022-02-25 11:29:09 +0100

    SplitStripBox

--- cbviz/cbviz/__init__.py
+++ cbviz/cbviz/__init__.py
@@ -5,6 +5,6 @@ The cbviz package provides classes and functions for visualization.
 
 from .kde import Ridge, SplitViolin
 from .xytools import XYview, XYpairs, XYzoom
-from .boxplot import StripBox
+from .boxplot import StripBox, SplitStripBox
 from .dotplot import Dotplot, Corrplot
 
